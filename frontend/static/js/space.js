f8ad97ad6b1256860ad7fae28c88af0240ffe049
Richard Jeremiah Cowans <rcowans11@gmail.com>
2019-11-21 23:00:13 -0500

    Fixed Background Stars and Added Planets

--- frontend/static/js/space.js
+++ frontend/static/js/space.js
@@ -1,24 +1,26 @@
 function prepareCalc() {
-    let h = window.innerHeight;
-    let w = window.innerWidth;
+    let h = window.innerHeight -20;
+    let w = window.innerWidth-20;
+    let b = (h * w );
+    let a = Math.floor(b*.75);
     let randdict = {};
-    return calculate(randdict, h, w, 0);
-
+    randomPos = calculate(randdict, h, w, 0,a);
+    addingToList(randomPos);
+    return false;
 }
 
 function addPhoto(x, y) {
-    cord = x.toString() + y.toString();
     var z = document.createElement("IMG");
-    z.setAttribute("src", "static/spec.jpg");
-    z.setAttribute("width", "304");
-    z.setAttribute("height", "228");
-    z.setAttribute("alt", "Testing");
-    z.setAttribute("id", cord);
+    z.setAttribute("src", "static/spec.png");
+    z.setAttribute("width", "20");
+    z.setAttribute("height", "20");
+    z.setAttribute("alt", "Star_Cord"+x.toString()+"_"+y.toString());
     $("img").last().offset({top: y, left: x});
+    //$("img").last().css();
     document.body.appendChild(z)
 }
 
-function addingToList() {
+function addingToList(test) {
     for (x = 0; x < Object.keys(test).length; x++) {
         if (Number(test[x])) {
             //console.log(x);
@@ -28,13 +30,14 @@ function addingToList() {
 
 }
 
-function calculate(randdict, h, w, n) {
-    while (n < 1000) {
+function calculate(randdict, h, w, n,a) {
+    while (n < a) {
         let tempX = Math.floor(Math.random() * w);
         let tempY = Math.floor(Math.random() * h);
-        if (randdict[tempX] !== tempY)
+        if (randdict[tempX] !== tempY) {
             randdict[tempX] = tempY;
-        n++;
+            n++
+        }
     }
     return randdict;
 }
