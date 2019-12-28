116cf043445d210826949f9a4bfd2e3c54df114f
Richard Jeremiah Cowans <rcowans11@gmail.com>
2019-12-28 17:53:56 -0500

    Improving the Curved words using JS Part 2

--- frontend/static/js/space.js
+++ frontend/static/js/space.js
@@ -4,9 +4,9 @@ function adjustPlanets() {
     h = window.innerHeight -20;
     w = window.innerWidth - 20;
     $(".GitHub").offset({top:h/2-50,left: 20});
-    $("#wrapGitHub").offset({top:(h/2) + 188,left: -125});
+    $("#wrapGitHub").offset({top:(h/2) +  160,left: -125});
     $(".AboutButton").offset({top:h-150,left: w/2 });
-    $("#wrapAboutMe").offset({top:h-200,left: (w/2)-175});
+    $("#wrapAboutMe").offset({top:h-120,left: (w/2)-175});
     $(".Intro").offset({top:(h/2) - 400 ,left:w/2 - 200 });
     $(".contactButton").offset({top:(h/2) - 50,left: w - 200 });
     arcem();
