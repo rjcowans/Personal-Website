c9fdf1f95c67f20c14e2a428925ce5896bb77ac7
Richard Jeremiah Cowans <rcowans11@gmail.com>
2019-12-27 21:54:14 -0500

    Image size fix and offsets using jquery

--- frontend/static/js/space.js
+++ frontend/static/js/space.js
@@ -7,7 +7,7 @@ function adjustPlanets() {
     h = window.innerHeight -20;
     w = window.innerWidth - 20;
     $(".GitHub").offset({top:h/2,left: 20});
-    $(".AboutButton").offset({top:h-100,left: w/2 });
+    $(".AboutButton").offset({top:h-150,left: w/2 });
     $(".Intro").offset({top:(h/2) - 400 ,left:w/2 - 200 });
-    $(".contactButton").offset({top:(h/2) - 50,left: w - 100 });
+    $(".contactButton").offset({top:(h/2) - 50,left: w - 200 });
 }
